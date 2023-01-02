b416db4d88c519efe4b48f8547a02984b4823d89
Hossam Khair <hossam.khir1@outlook.com>
2023-01-02 14:09:45 +0200

    feat: learned about LINQ approach to reverse strings

--- csharp/reverse-string/ReverseString.cs
+++ csharp/reverse-string/ReverseString.cs
@@ -1,12 +1,14 @@
 using System;
-
+using System.Linq;
 public static class ReverseString
 {
-    public static string Reverse(string input)
-    {
-        // throw new NotImplementedException("You need to implement this function.");
-        char[] arr = input.ToCharArray();
-        Array.Reverse(arr);
-        return String.Concat(arr);
-    }
+    // NOTE: LINQ approach (easier readability)
+    public static string Reverse(string input) => new(input.Reverse().ToArray());
+    // {
+    //     // throw new NotImplementedException("You need to implement this function.");
+    //     NOTE: Array.Reverse approach (best performance)
+    //     char[] arr = input.ToCharArray();
+    //     Array.Reverse(arr);
+    //     return String.Concat(arr);
+    // }
 }
